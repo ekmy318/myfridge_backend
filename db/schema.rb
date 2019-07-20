c566d0073448556f31f228ddf97e08a4ae0e74b4
ekmy318 <eunice.xp@gmail.com>
2019-07-19 23:24:21 -0400

    Connect Groceries and User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_022515) do
+ActiveRecord::Schema.define(version: 2019_07_20_032237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_07_20_022515) do
     t.money "price", scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_groceries_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_07_20_022515) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "groceries", "users"
 end
