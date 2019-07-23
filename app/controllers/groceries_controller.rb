a4b8b2f8d546dbbbc48f69fe0b06bbb55c9cd470
ekmy318 <eunice.xp@gmail.com>
2019-07-23 13:15:33 -0400

    update groceries controller

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -48,6 +48,6 @@ class GroceriesController < OpenReadController
 
   # Only allow a trusted parameter "white list" through.
   def grocery_params
-    params.require(:grocery).permit(:name, :expiration_date, :quantity, :price)
+    params.require(:grocery).permit(:name, :expiration_date, :quantity, :price, :user_id)
   end
 end
