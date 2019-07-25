0797e183ff73e00ca27100a3d61d648e8bc37c28
ekmy318 <eunice.xp@gmail.com>
2019-07-25 16:18:03 -0400

    Add days_till method

--- app/serializers/grocery_serializer.rb
+++ app/serializers/grocery_serializer.rb
@@ -2,9 +2,20 @@
 
 class GrocerySerializer < ActiveModel::Serializer
   belongs_to :user
-  attributes :id, :name, :expiration_date, :quantity, :price, :notes, :editable
+  attributes :id, :name, :expiration_date, :quantity, :price, :notes, :editable, :days_until
 
   def editable
     scope == object.user
+  end
+
+  def days_until
+    today = Date.new(Date.today.year, Date.today.month, Date.today.day)
+    if object.expiration_date == nil
+      return 'No date provided'
+    else
+      date_till = Date.new(object.expiration_date.year, object.expiration_date.month, object.expiration_date.day)
+    days = (date_till - today)
+    days.to_i
+    end
   end
 end
