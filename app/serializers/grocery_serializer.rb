20c0d1c81a01785859f392106acd2088a039f8ca
ekmy318 <eunice.xp@gmail.com>
2019-09-10 15:59:43 -0400

    Clean up serializer

--- app/serializers/grocery_serializer.rb
+++ app/serializers/grocery_serializer.rb
@@ -2,33 +2,35 @@
 
 class GrocerySerializer < ActiveModel::Serializer
   belongs_to :user
-  attributes :id, :name, :expiration_date, :quantity, :price, :notes, :editable, :days_until, :date_format
+  attributes :id, :name, :expiration_date, :quantity, :price, :notes,
+             :editable, :days_until, :date_format
+  Time.zone = 'America/New_York'
 
   def editable
     scope == object.user
   end
 
   def days_until
-    today = Date.new(Date.today.year, Date.today.month, Date.today.day)
-    if object.expiration_date == nil
-      return 'No date provided'
+    today = Date.new(Date.current.year, Date.current.month, Date.current.day)
+    if object.expiration_date.nil?
+      'No date provided'
     else
       date_till = Date.new(object.expiration_date.year, object.expiration_date.month, object.expiration_date.day)
-    days = (date_till - today)
-    if days.to_i.negative?
-      return 'Expired!'
-    elsif days.to_i.zero?
-      return 'Expires today!'
-    elsif days.to_i == 1
-      return 'Expires tomorrow!'
-    else days.to_i
-    end
+      days = (date_till - today)
+      if days.to_i.negative?
+        'Expired!'
+      elsif days.to_i.zero?
+        'Expires today!'
+      elsif days.to_i == 1
+        'Expires tomorrow!'
+      else days.to_i
+      end
     end
   end
 
   def date_format
-    if object.expiration_date == nil
-      return 'No date provided'
+    if object.expiration_date.nil?
+      'No date provided'
     else object.expiration_date.to_formatted_s(:long)
     end
   end
